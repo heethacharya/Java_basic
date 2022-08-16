0f12de1bc7f4d0ee3b36d499117149e74909bda2
heethacharya <acharya.heeth@gmail.com>
2022-08-15 22:36:34 -0400

    My second commit

--- src/training/Java_basic.java
+++ src/training/Java_basic.java
@@ -1,4 +1,39 @@
 package training;
 
+import java.sql.SQLOutput;
+
 public class Java_basic {
+
+    int roll = 5; // This is class level variable
+    String newName = "Google";
+
+
+    public static void main(String args[])throws Exception{
+        Java_basic obj = new Java_basic(); // this is creating object of class(it is blueprint of the class)
+
+        obj.add(10,10);
+        System.out.println(obj.roll);
+
+    }
+
+
+
+
+
+// This is the method
+    public void add(int a,int b){
+        String name_add = "Hetal"; // This is string type method level variable
+        int sum = a+b;
+
+        System.out.println(sum);
+
+    }
+    public void subtract(){
+        String name = "Acharya"; // This is string type method level variable
+        int street = 3;
+
+        //System.out.println(name_add);
+        System.out.println(newName);
+
+    }
 }
--- src/training/Java_loop.java
+++ src/training/Java_loop.java
@@ -1,4 +1,24 @@
 package training;
 
 public class Java_loop {
+
+
+
+
+    public void add(){
+        System.out.println("I am adding two elements");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
